0ce6a13d5f1d1eb58b88e750e98d12108bbcaff4
luciano.fagundes <luciano.fagundes@softplan.com.br>
2020-08-01 02:33:30 -0300

    Altera descriçao do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Criado por <a href="https://github.com/lucianojec">Luciano Oliveira</a> durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
